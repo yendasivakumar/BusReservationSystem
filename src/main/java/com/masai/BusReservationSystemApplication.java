ba7688205fb0cdd0269b27f2b1e92666640eb8cb
sivakumaryenda <sivakumaryenda@gmail.com>
2022-09-27 20:12:10 +0530

    modified

--- src/main/java/com/masai/BusReservationSystemApplication.java
+++ src/main/java/com/masai/BusReservationSystemApplication.java
@@ -11,6 +11,7 @@ public class BusReservationSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BusReservationSystemApplication.class, args);
+		System.out.println("main");
 	}
 
 }
